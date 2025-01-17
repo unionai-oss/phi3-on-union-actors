5798ac3dfabe2961498c2cc6c7a82f2563bbb78d
Sage Elliott <hello@sageelliott.com>
2025-01-16 19:23:10 -0800

    update workflow to call download tasks

--- workflows/phi3_response_wf.py
+++ workflows/phi3_response_wf.py
@@ -1,13 +1,17 @@
-from tasks.phi3 import inference
+from tasks.phi3 import inference, download_model
 from flytekit import workflow
 
 # --------------------------------
 # Workflow to run inference on multiple queries u
 # --------------------------------
-@workflow
-def phi_text_gen_workflow(query: str = "Hello") -> str:
 
-    result = inference(query=query)
+@workflow
+def phi_text_gen_workflow(query: str = "Hello",
+                        model_name: str = "microsoft/Phi-3-mini-128k-instruct") -> str:
+    model_cache_path = download_model(model_name=model_name)
+    result = inference(query=query,
+                       model_name=model_name,
+                       model_cache_path=model_cache_path)
 
     return result
 
